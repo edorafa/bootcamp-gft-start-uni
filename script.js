bf2713dd96da54d21c7aebc01027dde1a5df8203
Rafael Sevilha <54326621+edorafa@users.noreply.github.com>
2021-01-18 10:46:40 -0300

    cactus iteration

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
 let isJumping = false;
 
 function handleKeyup(event) {
@@ -35,6 +36,27 @@ function jump () {
     }, 20)
 }
 
+function createCactus(){
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    let ramdomTime = Math.random() * 6000;
 
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
 
+    let leftInterval = setInterval(() => {
+        if (cactusPosition < -60) {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 20)
+
+    setTimeout(createCactus, ramdomTime);
+}
+
+createCactus();
 document.addEventListener('keyup',handleKeyup);
