57682d26538057ab34b2e1ad8375ce8ffed79fa8
Rafael Sevilha <54326621+edorafa@users.noreply.github.com>
2021-01-18 10:21:39 -0300

    dino iteration

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-//scripts
+const dino = document.querySelector('.dino');
+let isJumping = false;
+
+function handleKeyup(event) {
+    if (event.keyCode === 32) // site keycode.info
+        if(!isJumping){
+            jump();
+        }         
+}
+
+function jump () {
+    let position = 0;
+    isJumping = true;
+
+    let upInterval = setInterval(() => {
+        if(position >= 150) {
+            clearInterval(upInterval);
+        
+
+        //Descendo
+        let downInterval = setInterval(() => {
+            if (position <= 0) {
+                clearInterval(downInterval);
+                isJumping = false;
+            }else {
+                position -= 20;
+                dino.style.bottom = position + 'px';
+            }
+        }, 20);
+
+        }else {
+            position += 20;
+            dino.style.bottom = position + 'px';
+        }
+    }, 20)
+}
+
+
+
+document.addEventListener('keyup',handleKeyup);
